fda8bd8decc4cd4723b18bf16b1b8804ee539514
Stefan Gustavson <stefan.gustavson@gmail.com>
2016-06-08 13:21:03 +0200

    Updated header to match .c file

--- src/noise1234.h
+++ src/noise1234.h
@@ -1,7 +1,7 @@
 // noise1234
 //
 // Author: Stefan Gustavson, 2003-2005
-// Contact: stegu@itn.liu.se
+// Contact: stefan.gustavson@liu.se
 //
 // This code was GPL licensed until February 2011.
 // As the original author of this code, I hereby
@@ -11,28 +11,28 @@
 // appreciate being told where this code finds any use,
 // but you may do as you like.
 
-/** \file
-		\brief Declares the "noise1" through "noise4" functions for Perlin noise.
-		\author Stefan Gustavson (stegu@itn.liu.se)
-*/
-
 /*
- * This is a backport to C of my improved noise class in C++.
+ * This implementation is "Improved Noise" as presented by
+ * Ken Perlin at Siggraph 2002. The 3D function is a direct port
+ * of his Java reference code which was once publicly available
+ * on www.noisemachine.com (although I cleaned it up, made it
+ * faster and made the code more readable), but the 1D, 2D and
+ * 4D functions were implemented from scratch by me.
+ *
+ * This is a backport to C of my improved noise class in C++
+ * which was included in the Aqsis renderer project.
  * It is highly reusable without source code modifications.
  *
- * Note:
- * Replacing the "float" type with "double" can actually make this run faster
- * on some platforms. A templatized version of Noise1234 could be useful.
  */
-
-/** 1D, 2D, 3D and 4D float Perlin noise, SL "noise()"
+ 
+/** 1D, 2D, 3D and 4D float Perlin noise
  */
 extern float noise1( float x );
 extern float noise2( float x, float y );
 extern float noise3( float x, float y, float z );
 extern float noise4( float x, float y, float z, float w );
 
-/** 1D, 2D, 3D and 4D float Perlin periodic noise, SL "pnoise()"
+/** 1D, 2D, 3D and 4D float Perlin periodic noise
  */
 extern float pnoise1( float x, int px );
 extern float pnoise2( float x, float y, int px, int py );
