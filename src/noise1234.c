cfc7ff3c9e5c60d6bc33e213fecb002a872d9a9d
Stefan Gustavson <stefan.gustavson@gmail.com>
2016-06-08 13:19:05 +0200

    Cosmetic bug in FASTFLOOR

--- src/noise1234.c
+++ src/noise1234.c
@@ -1,7 +1,7 @@
 // noise1234
 //
 // Author: Stefan Gustavson, 2003-2005
-// Contact: stegu@itn.liu.se
+// Contact: stefan.gustavson@liu.se
 //
 // This code was GPL licensed until February 2011.
 // As the original author of this code, I hereby
@@ -11,31 +11,26 @@
 // appreciate being told where this code finds any use,
 // but you may do as you like.
 
-/** \file
-		\brief Implements the noise1 to noise4 functions for Perlin noise.
-		\author Stefan Gustavson (stegu@itn.liu.se)
-*/
-
 /*
  * This implementation is "Improved Noise" as presented by
  * Ken Perlin at Siggraph 2002. The 3D function is a direct port
- * of his Java reference code available on www.noisemachine.com
- * (although I cleaned it up, made it faster and made the code
- * more readable), but the 1D, 2D and 4D cases were implemented
- * from scratch by me.
+ * of his Java reference code which was once publicly available
+ * on www.noisemachine.com (although I cleaned it up, made it
+ * faster and made the code more readable), but the 1D, 2D and
+ * 4D functions were implemented from scratch by me.
  *
- * This is a backport to C of my improved noise class in C++.
+ * This is a backport to C of my improved noise class in C++
+ * which was included in the Aqsis renderer project.
  * It is highly reusable without source code modifications.
  *
  */
 
-
-#include	"noise1234.h"
+#include "noise1234.h"
 
 // This is the new and improved, C(2) continuous interpolant
 #define FADE(t) ( t * t * t * ( t * ( t * 6 - 15 ) + 10 ) )
 
-#define FASTFLOOR(x) ( ((x)>0) ? ((int)x) : ((int)x-1 ) )
+#define FASTFLOOR(x) ( ((int)(x)<(x)) ? ((int)x) : ((int)x-1 ) )
 #define LERP(t, a, b) ((a) + (t)*((b)-(a)))
 
 
