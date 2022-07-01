913bb68aae0b537f0e18d6e1e747e606e29412e5
Stefan Gustavson <stefan.gustavson@gmail.com>
2022-07-01 11:23:31 +0200

    Add files via upload

---
+++ src/srnoise8.h
@@ -0,0 +1,24 @@
+// armnoise8.h
+
+// Simplex Flow Noise written for 8-bit ARM CPUs, in "hardware-tuned C"
+// Speed is a priority. Noise quality is of secondary concern.
+// If you want pretty high-res pictures, this is not the code you seek.
+// However, if you want to draw animated fire on a low resolution
+// LED array with an Arduino, this code could be quite useful.
+
+// The input coordinates are 16-bit unsigned integers in 7.8 bits
+// representation: 0 to 32767 represent 0.0 to 127.9961 in steps
+// of 1/256, with the pattern making a seamless tiling wrap at 128.
+// The output value is a signed 8-bit integer in 1.7s representation:
+// -128 to 127 represents -1 to 0.9922 in steps of 1/128.
+// The output value stays within the range -1 to 1 by a small margin.
+
+// Author: Stefan Gustavson (stefan.gustavson@gmail.com)
+// Version 2022-06-30
+// Released under the terms of the MIT license.
+
+signed char fakesin(unsigned char x);
+
+signed char fakecos(unsigned char x);
+
+signed char srnoise8(unsigned short x, unsigned short y, unsigned char alpha);
