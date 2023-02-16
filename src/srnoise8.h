857c74c0848ecde28a8845305e53ae1f5feab769
Stefan Gustavson <stefan.gustavson@gmail.com>
2023-02-16 11:20:51 +0100

    clarified the difference to armnoise8

--- src/srnoise8.h
+++ src/srnoise8.h
@@ -6,6 +6,11 @@
 // However, if you want to draw animated fire on a low resolution
 // LED array with an Arduino, this code could be quite useful.
 
+// This function is very similar to "armnoise8", but it adds a
+// parameter "alpha" to animate the noise pattern. This happens
+// at a small cost in speed, so if you don't want the animation,
+// "armnoise8" is slightly faster.
+
 // The input coordinates are 16-bit unsigned integers in 7.8 bits
 // representation: 0 to 32767 represent 0.0 to 127.9961 in steps
 // of 1/256, with the pattern making a seamless tiling wrap at 128.
